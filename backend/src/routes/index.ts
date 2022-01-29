0c775ba38d4032f6c4344097363c09b1b5778f3c
Brennan Wilkes <brennan@codexwilkes.com>
2022-01-29 13:00:01 -0800

    feat(backend): Auth router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import apiV1Router from "./v1";
+import authRouter from "./auth";
 
 //Initialize main router object
 const mainRouter = Router();
@@ -7,6 +8,7 @@ const mainRouter = Router();
 //Here we can include static routes or API routes from other files
 mainRouter.use("/api", apiV1Router);
 mainRouter.use("/api/v1", apiV1Router);
+mainRouter.use("/auth", authRouter);
 
 //Include a basic route for testing
 mainRouter.get("/", (_req, res) => res.send("Welcome to DevExp"));
