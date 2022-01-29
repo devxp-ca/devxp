08409f340253c3e7ffa9f2141f9eee11e9f8644d
Brennan Wilkes <brennan@codexwilkes.com>
2022-01-29 12:13:02 -0800

    feat(backend): DB connection and config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,14 @@
 import RESTServer from "./server";
 import mainRouter from "./routes/index";
+import {mongoose} from "./database/connection";
 
 const server = new RESTServer();
 server.route("/", mainRouter);
-server.start();
+
+mongoose.connection.on(
+	"error",
+	console.error.bind(console, "connection error:")
+);
+mongoose.connection.once("open", function callback() {
+	server.start();
+});
