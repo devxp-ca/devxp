7092be87f704712e6d74c8d5fa70965f3b513bcd
Brennan Wilkes <brennan@codexwilkes.com>
2022-01-30 15:06:59 -0800

    feat(frontend): Basic config and new packages

---
+++ frontend/src/config.ts
@@ -0,0 +1,9 @@
+export interface Config {
+	BACKEND_URL: string;
+	AUTH_PATH: string;
+}
+
+export const CONFIG: Config = {
+	BACKEND_URL: "devxp-backend-hiq5lyfpqq-uw.a.run.app",
+	AUTH_PATH: "/auth"
+};
