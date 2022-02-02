a858d0052a9355de72eb68f0b978a35e4444883c
Derek Robinson <derek.robinson1020@gmail.com>
2022-02-02 15:31:31 -0800

    Adding footer to homepage

--- frontend/src/pages/homepage.tsx
+++ frontend/src/pages/homepage.tsx
@@ -3,16 +3,22 @@ import LoginWithGithub from "../components/loginWithGithub";
 import Navbar from "../components/Navbar";
 import Typography from "@mui/material/Typography";
 import MouseOverPopover from "../components/MouseOverPopover";
+import Footer from "../components/Footer";
+import Container from "@mui/material/Container";
 
 interface IProps {}
 interface IState {}
 export default class Homepage extends React.Component<IProps, IState> {
 	render() {
 		return (
-			<>
+			<Container maxWidth="lg">
 				<Navbar />
-				<MouseOverPopover text ='Hello World' popOverInfo="Hello World but in popover"/>
-			</>
+				<MouseOverPopover
+					text="Hello World"
+					popOverInfo="Hello World but in a popover box"
+				/>
+				<Footer />
+			</Container>
 		);
 	}
 }
