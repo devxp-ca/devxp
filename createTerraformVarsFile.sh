f0414cbbda5297e023ea5cedbdb5262d28b3300f
Brennan Wilkes <brennan@codexwilkes.com>
2022-02-06 00:25:19 -0800

    ci: Finished terraform CI/CD

--- createTerraformVarsFile.sh
+++ createTerraformVarsFile.sh
@@ -4,9 +4,5 @@ touch terraform.tfvars
 
 for env in "$@"
 do
-	echo "$env"
-	eval echo "\$env = "\\\"\$$env\\\" | wc -c
     eval echo "\$env = "\\\"\$$env\\\" >> terraform.tfvars
 done
-
-cat terraform.tfvars | wc -c
