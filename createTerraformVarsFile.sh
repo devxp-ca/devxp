00efbb8ac327a69885c97086b009f5cfd79c4917
Brennan Wilkes <brennan@codexwilkes.com>
2022-02-06 00:22:58 -0800

    ci: CI test

--- createTerraformVarsFile.sh
+++ createTerraformVarsFile.sh
@@ -4,5 +4,9 @@ touch terraform.tfvars
 
 for env in "$@"
 do
+	echo "$env"
+	eval echo "\$env = "\\\"\$$env\\\" | wc -c
     eval echo "\$env = "\\\"\$$env\\\" >> terraform.tfvars
 done
+
+cat terraform.tfvars | wc -c
