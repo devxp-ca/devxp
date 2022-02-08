3f03150a2217a5fc6e6196bb98d54122590259a2
Brennan Wilkes <bwilkes@uvic.ca>
2022-02-08 19:20:41 +0000

    feat(backend): Added provider DB schemas

--- backend/src/types/database.ts
+++ backend/src/types/database.ts
@@ -4,10 +4,14 @@ export type Doc<Base> = Base & Document;
 
 export const generateSchema = <Base extends Record<string, any>>(
 	base: Base
-) => {
+): Schema<Base, Model<Base>, Base> => {
 	const mappedBase: any = {};
 	Object.keys(base).map(key => {
 		mappedBase[key] = {type: typeof base[key]};
 	});
 	return new Schema<Base, Model<Base>, Base>(mappedBase);
 };
+
+export interface DatabaseModel<Base> {
+	toSchema(): Schema<Base, Model<Base>, Base>;
+}
--- backend/src/types/terraform.ts
+++ backend/src/types/terraform.ts
@@ -1,3 +1,5 @@
+import {DatabaseModel, generateSchema} from "./database";
+
 // ---------------------------------Variable---------------------------------- //
 export type variableType =
 	| string
@@ -34,6 +36,18 @@ export interface requiredProvider {
 	version: string;
 }
 export type namedRequiredProvider = named<requiredProvider, providerName>;
+
+export abstract class NamedRequiredProvider implements namedRequiredProvider {
+	source: string;
+	version: string;
+	name: providerName;
+	constructor(source: string, version: string, name: providerName) {
+		this.source = source;
+		this.version = version;
+		this.name = name;
+	}
+}
+
 export interface googleProvider extends named<requiredProvider, "google"> {
 	project?: string;
 	region?: string;
@@ -44,6 +58,36 @@ export const isGoogleProvider = (
 	provider: namedRequiredProvider
 ): provider is googleProvider => provider.name === "google";
 
+export class GoogleProvider
+	extends NamedRequiredProvider
+	implements googleProvider, DatabaseModel<googleProvider>
+{
+	project?: string;
+	region?: string;
+	zone?: string;
+	credentials?: string;
+	name: "google";
+	constructor(
+		source: string,
+		version: string,
+		project: string | null,
+		region: string | null,
+		zone: string | null,
+		credentials: string | null
+	) {
+		super(source, version, "google");
+		this.name = "google";
+		if (project) this.project = project;
+		if (region) this.region = region;
+		if (zone) this.zone = zone;
+		if (credentials) this.credentials = credentials;
+	}
+
+	toSchema() {
+		return generateSchema<googleProvider>(this);
+	}
+}
+
 export interface awsProvider extends named<requiredProvider, "aws"> {
 	region: string;
 	access_key: string;
@@ -52,6 +96,33 @@ export interface awsProvider extends named<requiredProvider, "aws"> {
 export const isAwsProvider = (
 	provider: namedRequiredProvider
 ): provider is awsProvider => provider.name === "aws";
+
+export class AwsProvider
+	extends NamedRequiredProvider
+	implements awsProvider, DatabaseModel<awsProvider>
+{
+	region: string;
+	access_key: string;
+	secret_key: string;
+	name: "aws";
+	constructor(
+		source: string,
+		version: string,
+		region: string,
+		access_key: string,
+		secret_key: string
+	) {
+		super(source, version, "aws");
+		this.name = "aws";
+		this.region = region;
+		this.access_key = access_key;
+		this.secret_key = secret_key;
+	}
+
+	toSchema() {
+		return generateSchema<awsProvider>(this);
+	}
+}
 
 // --------------------------------Backend----------------------------------- //
 
