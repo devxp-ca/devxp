419f48d8fb6e74074f350971dc5dd95b8020c3bb
Derek Robinson <derek.robinson1020@gmail.com>
2022-02-24 11:02:56 -0800

    Refactor loginWithGithub.tsx to FC

--- frontend/src/components/loginWithGithub.tsx
+++ frontend/src/components/loginWithGithub.tsx
@@ -4,19 +4,15 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import Button from "@mui/material/Button";
 import {CONFIG} from "../config";
 
-interface IProps {}
-interface IState {}
-export default class LoginWithGithub extends React.Component<IProps, IState> {
-	render() {
-		return (
-			<Button
-				color="inherit"
-				startIcon={<GitHubIcon />}
-				onClick={() =>
-					(window.location.href = `https://${CONFIG.BACKEND_URL}${CONFIG.AUTH_PATH}`)
-				}>
-				Login with GitHub
-			</Button>
-		);
-	}
+export default function LoginWithGithub() {
+	return (
+		<Button
+			color="inherit"
+			startIcon={<GitHubIcon />}
+			onClick={() =>
+				(window.location.href = `https://${CONFIG.BACKEND_URL}${CONFIG.AUTH_PATH}`)
+			}>
+			Login with GitHub
+		</Button>
+	);
 }
