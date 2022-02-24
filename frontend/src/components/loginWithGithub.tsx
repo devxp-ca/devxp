ceb4b77d116e670c76e4d31b24f1f7867de5fcda
Derek Robinson <derek.robinson1020@gmail.com>
2022-02-24 11:34:59 -0800

    change(frontend): Change login button to logout when user is already logged in

--- frontend/src/components/loginWithGithub.tsx
+++ frontend/src/components/loginWithGithub.tsx
@@ -3,16 +3,41 @@ import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import Button from "@mui/material/Button";
 import {CONFIG} from "../config";
+import {Cookies} from "react-cookie";
 
 export default function LoginWithGithub() {
+	//inside of the useState hook, check to see if the access_token is set in the cookies. If it is, then the user is already logged in.
+	const [isLoggedIn, setIsLoggedIn] = React.useState(() => {
+		const cookies = new Cookies();
+		const access_token = cookies.get("access_token");
+		if (access_token) {
+			return true;
+		} else {
+			return false;
+		}
+	});
+
+	const handleLogin = () => {
+		if (isLoggedIn) {
+			// set the state to logged out
+			setIsLoggedIn(false);
+			// Maybe: remove the access_token from the cookies
+			const cookies = new Cookies();
+			cookies.remove("access_token");
+		} else {
+			// set the state to logged in
+			setIsLoggedIn(true);
+			// call the API to get the user's profile
+			window.location.href = `https://${CONFIG.BACKEND_URL}${CONFIG.AUTH_PATH}`;
+		}
+	};
+
 	return (
 		<Button
 			color="inherit"
 			startIcon={<GitHubIcon />}
-			onClick={() =>
-				(window.location.href = `https://${CONFIG.BACKEND_URL}${CONFIG.AUTH_PATH}`)
-			}>
-			Login with GitHub
+			onClick={handleLogin}>
+			{isLoggedIn ? "Logout" : "Login with Github"}
 		</Button>
 	);
 }
