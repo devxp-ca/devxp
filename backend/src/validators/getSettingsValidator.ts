cd9870dac630b9e1b39ed5850df2b1583b41b6a1
Brennan Wilkes <brennan@codexwilkes.com>
2022-03-02 08:52:13 -0800

    feat(backend): Repo header validator Closes #152

---
+++ backend/src/validators/getSettingsValidator.ts
@@ -0,0 +1,18 @@
+import {header} from "express-validator";
+import {validationErrorHandler} from "../types/errorHandler";
+
+export default [
+	header("token")
+		.exists()
+		.trim()
+		.escape()
+		.isLength({min: 3})
+		.withMessage("Invalid authorization token"),
+	header("repo")
+		.exists()
+		.trim()
+		.isLength({min: 3})
+		.matches(/^[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+$/)
+		.withMessage("Invalid repo header"),
+	validationErrorHandler
+];
