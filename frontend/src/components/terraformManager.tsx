14dbfb1bcaa3782c7f98ad8f5a41d47949c92e94
Sabrina <36714185+skorsch@users.noreply.github.com>
2022-03-03 23:01:50 -0800

    Implemented multiple cards for previous instances, needs db testing

--- frontend/src/components/terraformManager.tsx
+++ frontend/src/components/terraformManager.tsx
@@ -44,37 +44,97 @@ export default function TerraformManager(props: {
 		setSelectNewInstance(true);
 	};
 
-	// returns a static example for now -- Create terraformInstanceCards with terraformDataSettings
-	const getPreviousInstances = () => {
-		const tempData: terraformDataSettings = {
-			repo: "test",
-			tool: "terraform",
-			settings: {
-				provider: "aws",
-				resources: [
-					{
-						type: "ec2",
-						id: "TEST-NAME",
-						ami: "ami-0892d3c7ee96c0bf7",
-						instance_type: "t2.medium"
-					},
-					{
-						type: "ec2",
-						id: "TEST-NAME",
-						ami: "ami-0892d3c7ee96c0bf7",
-						instance_type: "t2.medium"
-					}
-				]
-			}
-		};
+	const expandPreviousSettings = (
+		prevSettings: terraformDataSettings
+	): terraformDataSettings[] => {
+		if (!Boolean(prevSettings)) {
+			return [];
+		}
+		const collect: terraformDataSettings[] = [];
+
+		prevSettings.settings.resources.forEach(element => {
+			collect.push({
+				repo: prevSettings.repo,
+				tool: prevSettings.tool,
+				settings: {
+					provider: prevSettings.settings.provider,
+					resources: [element]
+				}
+			});
+		});
+
+		return collect;
+	};
+
+	const getCards = () => {
+		//array of terraformDataSettings with only one resource per index, used for tile generation
+		//expanding these to keep consistenty with the terraformDataSettings interface (instead of chopping it up)
+		const prevInstanceSettings = expandPreviousSettings(props.repoData);
 
 		return (
-			<Grid item>
-				<TerraformInstanceCard
-					cardData={tempData}
-					cardSize={defaultCardSize}
-					selectedRepo={props.selectedRepo}
-				/>
+			<Grid container spacing={4}>
+				<Grid item>
+					<Button
+						variant="outlined"
+						sx={{width: 3, height: defaultCardSize}}
+						onClick={props.backButton}>
+						<ArrowBackIcon />
+					</Button>
+				</Grid>
+				{/* New Terraform Instance */}
+				<Grid item>
+					<Card>
+						{!selectedNewInstance && (
+							<CardActionArea
+								onClick={selectedNewInstanceCallback}
+								sx={{
+									"&:hover": {
+										backgroundColor: `${currentTheme.palette.success.main}50`
+									}
+								}}>
+								<Grid
+									container
+									justifyContent="center"
+									alignItems="center"
+									sx={{
+										width: defaultCardSize / 2,
+										height: defaultCardSize,
+										border: `1px solid ${currentTheme.palette.success.main}`,
+										borderRadius: 1
+									}}>
+									<Grid item>
+										<AddIcon
+											sx={{
+												width: 75,
+												height: 75,
+												opacity: 1,
+												color: currentTheme.palette
+													.success.main
+											}}
+										/>
+									</Grid>
+								</Grid>
+							</CardActionArea>
+						)}
+						{/* TODO: bug: changing provider when option box is already opened doesn't refresh options*/}
+						{selectedNewInstance && (
+							<TerraformOptions
+								selectedRepo={props.selectedRepo}
+								globalProvider={selectedProvider}
+							/>
+						)}
+					</Card>
+				</Grid>
+				{/* Populate previous instances here */}
+				{prevInstanceSettings.map(cardSettings => (
+					<Grid item>
+						<TerraformInstanceCard
+							cardData={cardSettings}
+							cardSize={defaultCardSize}
+							selectedRepo={props.selectedRepo}
+						/>
+					</Grid>
+				))}
 			</Grid>
 		);
 	};
@@ -139,62 +199,7 @@ export default function TerraformManager(props: {
 					</Grid>
 				</FormControl>
 			</Grid>
-			<Grid container spacing={4}>
-				<Grid item>
-					<Button
-						variant="outlined"
-						sx={{width: 3, height: defaultCardSize}}
-						onClick={props.backButton}>
-						<ArrowBackIcon />
-					</Button>
-				</Grid>
-				{/* New Terraform Instance */}
-				<Grid item>
-					<Card>
-						{!selectedNewInstance && (
-							<CardActionArea
-								onClick={selectedNewInstanceCallback}
-								sx={{
-									"&:hover": {
-										backgroundColor: `${currentTheme.palette.success.main}50`
-									}
-								}}>
-								<Grid
-									container
-									justifyContent="center"
-									alignItems="center"
-									sx={{
-										width: defaultCardSize / 2,
-										height: defaultCardSize,
-										border: `1px solid ${currentTheme.palette.success.main}`,
-										borderRadius: 1
-									}}>
-									<Grid item>
-										<AddIcon
-											sx={{
-												width: 75,
-												height: 75,
-												opacity: 1,
-												color: currentTheme.palette
-													.success.main
-											}}
-										/>
-									</Grid>
-								</Grid>
-							</CardActionArea>
-						)}
-						{/* TODO: bug: changing provider when option box is already opened doesn't refresh options*/}
-						{selectedNewInstance && (
-							<TerraformOptions
-								selectedRepo={props.selectedRepo}
-								globalProvider={selectedProvider}
-							/>
-						)}
-					</Card>
-				</Grid>
-				{/* Populate previous instances here */}
-				{getPreviousInstances()}
-			</Grid>
+			{getCards()}
 		</Box>
 	);
 }
