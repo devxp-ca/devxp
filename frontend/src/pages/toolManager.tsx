9ec6e1d2286196a14e36d5233143f369d927371f
Brennan Wilkes <brennan@codexwilkes.com>
2022-03-16 18:44:06 -0700

    chore(frontend): Removed example

--- frontend/src/pages/toolManager.tsx
+++ frontend/src/pages/toolManager.tsx
@@ -17,7 +17,6 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import GenericModal from "../components/GenericModal";
-import DynamoDb from "../components/resources/DynamoDb";
 
 import terraformPNG from "../assets/Terraform_Vertical.png";
 
@@ -228,7 +227,6 @@ export default function ToolManager() {
 								repoData={selectedRepoData}
 							/>
 						)}
-						<DynamoDb onChange={console.log} />
 					</Grid>
 				</Box>
 			</Box>
