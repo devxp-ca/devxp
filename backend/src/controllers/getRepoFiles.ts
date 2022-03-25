844fd1368b33b5d5ffcca719bf071613d8b9af72
Brennan Wilkes <brennan@codexwilkes.com>
2022-03-25 08:55:42 -0700

    feat(backend): Basic get files controller

---
+++ backend/src/controllers/getRepoFiles.ts
@@ -0,0 +1,25 @@
+import {Request, Response} from "express";
+import getFiles from "../githubapi/getFiles";
+import {internalErrorHandler} from "../types/errorHandler";
+
+export const getRepoFiles = (req: Request, res: Response) => {
+	getFiles(req.headers.token as string, req.query.repo as string)
+		.then(files => {
+			res.json({
+				files
+			});
+		})
+		.catch(() =>
+			getFiles(
+				req.headers.token as string,
+				req.query.repo as string,
+				"master"
+			)
+		)
+		.then(files => {
+			res.json({
+				files
+			});
+		})
+		.catch(internalErrorHandler(req, res));
+};
