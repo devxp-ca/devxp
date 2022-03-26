009032d290957a0fdf3565e8db4474d74750fe87
Derek Robinson <derek.robinson1020@gmail.com>
2022-03-25 19:19:52 -0700

    change(frontend): footer shows up on homepage now

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,21 +1,38 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
 import MuiLink from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 
 export default function Footer() {
 	return (
-		<Box
+		<Grid
+			container
+			direction="column"
 			sx={{
-				position: "sticky",
-				marginTop: -5
+				backgroundColor: "primary",
+				position: "fixed",
+				left: 0,
+				bottom: 0,
+				right: 0
 			}}>
 			<Typography variant="body2" color="text.secondary" align="center">
+				<MuiLink
+					href="https://github.com/devxp-ca/devxp/wiki"
+					color="inherit"
+					target="_blank"
+					rel="noopener noreferrer">
+					Wiki
+				</MuiLink>
+			</Typography>
+			<Typography variant="body2" color="text.secondary" align="center">
 				{"Copyright © "}
-				<MuiLink color="inherit">DevXP</MuiLink>{" "}
+				<MuiLink href="https://github.com/devxp-ca" color="inherit">
+					DevXP
+				</MuiLink>{" "}
 				{new Date().getFullYear()}
 				{"."}
 			</Typography>
-		</Box>
+		</Grid>
 	);
 }
