97ce20d38d681cbdd312c108954357a87dd3fca3
Derek Robinson <derek.robinson1020@gmail.com>
2022-04-14 11:15:10 -0700

    change(front): refactor homepage to FC

--- frontend/src/pages/homepage.tsx
+++ frontend/src/pages/homepage.tsx
@@ -9,80 +9,76 @@ import ProductPage from "../components/ProductPage";
 import Footer from "../components/Footer";
 import Grid from "@mui/material/Grid";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import {darkTheme} from "../style/themes";
 import {Element, scroller} from "react-scroll";
 
-//Trigger deployment
-interface IProps {}
-interface IState {}
-export default class Homepage extends React.Component<IProps, IState> {
-	render() {
-		return (
-			<ThemeProvider theme={darkTheme}>
+export default function Homepage() {
+	//True if screen width > 600px, else false
+	const isMobile = useMediaQuery("(max-width:600px)");
+
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<Grid
+				container
+				direction="column"
+				sx={{
+					backgroundColor: "primary.dark",
+					width: "auto"
+				}}>
 				<Grid
 					container
 					direction="column"
 					sx={{
+						minHeight: "100vh",
 						backgroundColor: "primary.dark",
 						width: "auto"
 					}}>
 					<Grid
-						container
-						direction="column"
+						item
 						sx={{
-							minHeight: "100vh",
-							backgroundColor: "primary.dark",
-							width: "auto"
+							width: "100%",
+							paddingLeft: isMobile === true ? 0 : 6,
+							paddingRight: isMobile === true ? 0 : 6
 						}}>
-						<Grid
-							item
-							sx={{
-								width: "100%",
-								paddingLeft: 6,
-								paddingRight: 6
-							}}>
-							<Navbar />
-						</Grid>
-						<Grid
-							item
-							container
-							sx={{
-								flexGrow: 1,
-								paddingTop: 2,
-								paddingBottom: 3
-							}}>
-							<React.Suspense
-								fallback={
-									<Grid
-										item
-										sx={{
-											width: "100%",
-											backgroundColor: "primary.dark",
-											zIndex: 3
-										}}
-									/>
-								}>
-								<MainLandingVisual
-									onClick={() => {
-										scroller.scrollTo(
-											"DevXP-Product-Title",
-											{
-												duration: 1000,
-												delay: 100,
-												smooth: true
-											}
-										);
+						<Navbar />
+					</Grid>
+					<Grid
+						item
+						container
+						sx={{
+							flexGrow: 1,
+							paddingTop: 2,
+							paddingBottom: 3
+						}}>
+						<React.Suspense
+							fallback={
+								<Grid
+									item
+									sx={{
+										width: "100%",
+										backgroundColor: "primary.dark",
+										zIndex: 3
 									}}
 								/>
-							</React.Suspense>
-						</Grid>
+							}>
+							<MainLandingVisual
+								onClick={() => {
+									scroller.scrollTo("DevXP-Product-Title", {
+										duration: 1000,
+										delay: 100,
+										smooth: true
+									});
+								}}
+							/>
+						</React.Suspense>
 					</Grid>
-					<Element name="DevXP-Product-Title">
-						<ProductPage />
-					</Element>
-					<Footer />
 				</Grid>
-			</ThemeProvider>
-		);
-	}
+				<Element name="DevXP-Product-Title">
+					<ProductPage />
+				</Element>
+				<Footer />
+			</Grid>
+		</ThemeProvider>
+	);
 }
